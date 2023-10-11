a87fe5db5b2e4c432b9d7cb4ea9dd3220cabe65e
Egehan Daştan <87079212+egehandastan@users.noreply.github.com>
2023-10-11 09:57:09 +0300

    .

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,76 +1,135 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/categories.dart';
+import 'package:shopping_list_app/models/category.dart';
 
-class NewItem extends StatefulWidget{
+class NewItem extends StatefulWidget {
   const NewItem({super.key});
   @override
   State<StatefulWidget> createState() {
-   
-   return _NewItemState();
+    return _NewItemState();
+  }
+}
+
+class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
+
+  void _saveItem() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print(_enteredName);
+      print(_enteredQuantity);
+      print(_selectedCategory);
+    }
   }
 
-}
-class _NewItemState extends State<NewItem>{
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add a new item'),
       ),
-      body:  Padding(
-        padding:  const EdgeInsets.all(12),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
                 maxLength: 50,
-                decoration: InputDecoration(
-                  label: Text('Name')
-                ),
-                validator: (value){
-                  return 'Demo...';
+                decoration: const InputDecoration(label: Text('Name')),
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be 1 and 50 characters';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _enteredName = value!;
                 },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                 Expanded(child: 
-                  TextFormField(
-                    decoration:const InputDecoration(
-                      label: Text('Quantity'),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      keyboardType: TextInputType.number,
+                      initialValue: _enteredQuantity.toString(),
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be 1 and 50 characters';
+                        }
+                        return null;
+                      },
+                      onSaved: (value){
+                        _enteredQuantity = int.parse(value!);
+                      },
                     ),
-                    initialValue: '1',
-                  ),),
-                  const SizedBox(width: 8,),
-                Expanded(child: 
-                  DropdownButtonFormField(items: [
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                      child: DropdownButtonFormField(
+                        value: _selectedCategory,
+                        items: [
                     for (final category in categories.entries)
                       DropdownMenuItem(
-                       value: category.value,
-                      child:  Row(
-                       children: [
-                         Container(
-                          width: 16,
-                          height: 16,
-                          color: category.value.color,
-                        ),
-                        const SizedBox(width: 6,),
-                        Text(category.value.title),
-                       ],
-                        
-                      )
-                      ),
-                  ], onChanged: (value){}))
-
-              ],),
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(
+                                width: 6,
+                              ),
+                              Text(category.value.title),
+                            ],
+                          )),
+                  ], onChanged: (value) {
+                    setState(() {
+                      _selectedCategory = value!;
+                    });
+                    
+                  }
+                  ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        _formKey.currentState!.reset();
+                      },
+                      child: const Text('Reset')),
+                  ElevatedButton(
+                      onPressed: _saveItem, child: const Text('Add Item'))
+                ],
+              )
             ],
           ),
         ),
-        ),
+      ),
     );
   }
-
-
 }
